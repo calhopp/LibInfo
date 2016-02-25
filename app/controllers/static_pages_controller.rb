f3f12b707e6050e9461639443604681c09612dc2
calhopp <calhopp@gmail.com>
2016-02-25 13:05:56 -0600

    Changed from embed to RSS feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+
 	def home
+		url = 'http://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss'
+		@feed = Feedjira::Feed.fetch_and_parse url
+		@title = @feed.title
+		@feedurl = @feed.url
+		@entries = @feed.entries
 	end
+
 end
